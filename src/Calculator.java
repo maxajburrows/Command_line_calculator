987056e4c80c610601f5865d0e18922668149033
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-05-02 07:02:46 +0200

    Create a separate method for receiving inputs from the user

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 
 public  class Calculator {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the first number");
-        double number1 = scanner.nextDouble();
-        System.out.println("Enter the second number");
-        double number2 = scanner.nextDouble();
+        double number1 = getNumber();
+        double number2 = getNumber();
         scanner.close();
         double sum = addNumbers(number1,number2);
         printResult(number1, number2, sum);
 
     }
         // to get 2 numbers from user
+
+    public static double getNumber(){
+        System.out.println("Enter a number");
+        double num = scanner.nextDouble();
+        return num;
+    }
      public static double addNumbers(double number1, double number2){
 
 
